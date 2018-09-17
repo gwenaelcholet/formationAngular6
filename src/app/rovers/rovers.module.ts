05a610c0a4381b842a60dd5957e48ed01061d764
Gwenael Cholet <gcholet@sada.fr>
2018-09-17 14:26:42 +0200

    Addition of rover items
    Use an component template passing input from parent

--- src/app/rovers/rovers.module.ts
+++ src/app/rovers/rovers.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { RoversRoutingModule } from './rovers-routing.module';
 import { PageRoversListComponent } from './page-rovers-list/page-rovers-list.component';
+import { RoverItemComponent } from './rover-item/rover-item.component';
 
 @NgModule({
   imports: [
     CommonModule,
     RoversRoutingModule
   ],
-  declarations: [PageRoversListComponent]
+  declarations: [PageRoversListComponent, RoverItemComponent]
 })
 export class RoversModule { }
